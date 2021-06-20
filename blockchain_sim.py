f1fdbf9902f68a19c5e2923e04c27d8d7fd68be5
SuperbSaud <48680823+SuperbSaud@users.noreply.github.com>
2021-06-20 22:17:52 +0530

    Transactions added. Merkle root to be added.

--- blockchain_sim.py
+++ blockchain_sim.py
@@ -5,32 +5,47 @@
 """
 
 # Importing modules
-import hashlib                 #hashlib for hashing algorithms
-import datetime as date        #datetime for dates
+import hashlib                 # hashlib for hashing algorithms
+import datetime as date        # datetime for dates
+import random                  # random for randomizing numbers for transactions 
+# import math                    # math for logarithmic function used in merkle root hashing function              
 
 class Block():
-    def __init__(self, height, timestamp, data, prev_hash):
+    """
+        A certain block consists of the following entities:
+        + Block height
+        + Timestamp
+        + Previous block hash
+        + Transactions
+        + Transactions counter
+    """
+    def __init__(self, height, timestamp, prev_hash, txs):
         self.height = height
         self.timestamp = timestamp
-        self.data = data
         self.prev_hash = prev_hash
         self.hash = self.hash_block()
+        self.transactions = txs
+        self.tx_ctr = len(txs)
+        # self.merkle = self.merkel_root_hash(self.tx_ctr)
 
     def hash_block(self):
-        msg = (str(self.height) + str(self.timestamp) + str(self.data) + str(self.prev_hash)).encode()
+        msg = (str(self.height) + str(self.timestamp) + str(self.prev_hash)).encode()
         sha = hashlib.sha256(msg)
         return sha.hexdigest()
 
+    # def merkel_root_hash():
+    #     pass
+
 def create_genesis_block():
     # Manually construct a block with height zero and arbitrary previous hash
-    return Block(0, date.datetime.now(), "Genesis Block", "0")
+    return Block(0, date.datetime.now(), "0", [random.randint(1,1000) for _ in range(10)])
 
 def next_block(last_block):
     this_height = last_block.height + 1
     this_timestamp = date.datetime.now()
-    this_data = "Hello. I am Block #" + str(this_height)
     this_hash = last_block.hash
-    return Block(this_height, this_timestamp, this_data, this_hash)
+    this_txs = [random.randint(1,1000) for _ in range(10)]
+    return Block(this_height, this_timestamp, this_hash, this_txs)
 
 if __name__=="__main__":
     
@@ -48,6 +63,14 @@ if __name__=="__main__":
         previous_block = block_to_add
 
         # Showing block information
-        print(f"Block #{i} has been added to the blockchain!")
-        print(f"Hash: {block_to_add.hash}")
-        print(f"Block Height: {block_to_add.height}")
+        print(
+            (f"\nBlock #{i} has been added to the blockchain!\n"),
+            ("\t\t{}\n".format("_"*100)),
+            (f"\t\tHash: {block_to_add.hash}"),
+            (f"| Block Height: {block_to_add.height}"),
+            (f"| Timestamp: {block_to_add.timestamp}"),
+            (f"| Previous Root Hash: {block_to_add.prev_hash}"),
+            (f"| Transactions: {block_to_add.transactions}"),
+            (f"| Transaction Counter: {block_to_add.tx_ctr}"),
+            ("\t\t{}\n".format("_"*100))
+        )
